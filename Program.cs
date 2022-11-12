3a4ea686f026ed498a1c1c6bc52223030cc9cdd0
andrecarvalhas <andre.t.carvalhas@gmail.com>
2022-11-12 11:32:00 +0000

    Comentário Extra

--- Program.cs
+++ Program.cs
@@ -75,6 +75,7 @@
             students.Add("LEONARDO Andrade");
 
             //Get Random Value to find student
+            //Este é outro comentário que me tinha esquecido. 
             var random = new Random(DateTime.Now.Millisecond);
             var  index = random.Next(students.Count);
             
